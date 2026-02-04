1b7cd31da8fca48d3bc6753e56dfa6a355cbf9f7
akintarman-eng <akintarman@gmail.com>
2026-02-04 15:41:14 +0300

    app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ if ogr_no_input:
         
         ihlaller = st.multiselect(
             "İhlal Türlerini Seçiniz:",
-            ["Saç-Sakal", "Kıyafet", "Makyaj", "Takı"]
+            ["Saç", "Sakal", "Kıyafet", "Makyaj", "Takı"]
         )
         notlar = st.text_input("Ek Not:")
         
